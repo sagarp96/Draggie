f09155d5f5a56ca660da96625e3f7b28036073b8
sagarp96 <sagarcool2345@gmail.com>
2025-08-26 20:22:20 +0530

    feat: Enhance TaskCard with action buttons and metadata

--- src/components/tasks.tsx
+++ src/components/tasks.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { useDraggable } from "@dnd-kit/core";
 import { Task } from "./types";
+import { Trash2 } from "lucide-react";
+import { EditTaskBTN } from "./EditTaskBTN";
+import { Button } from "@/components/ui/button";
 
 type TaskCardProps = {
   task: Task;
@@ -25,8 +28,20 @@ export function TaskCard({ task }: TaskCardProps) {
       className="cursor-grab rounded-lg p-4 shadow-sm hover:shadow-md mt-3 bg-indigo-500 touch-manipulation select-none"
       style={style}
     >
-      <h3 className=" font-medium text-neutral-100">{task.title}</h3>
-      <p className="mt-2 text-sm text-neutral-400">{task.description}</p>
+      <h3 className="font-medium text-neutral-100 text-center mb-10">
+        {task.title}
+      </h3>
+      <br />
+      <footer className="text-center">
+        <p className="text-xs text-neutral-400">Created by:Sagar panwar</p>
+        <p className="text-xs text-neutral-400">Last Updated:12:00pn</p>
+      </footer>
+      <div className="flex justify-center ">
+        <Button variant="ghost">
+          <Trash2 className="mr-2 h-4 w-4 text-red-500" />
+        </Button>
+        <EditTaskBTN />
+      </div>
     </div>
   );
 }
