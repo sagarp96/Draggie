f6a4dc6cf0fb2ead5a93c929867330473c255cfe
sagarp96 <sagarcool2345@gmail.com>
2025-09-02 16:56:49 +0530

    Login page changes

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -118,18 +118,6 @@ export function LoginForm({
           </div>
         </div>
       </form>
-
-      <div className="text-muted-foreground text-center text-xs text-balance">
-        By clicking continue, you agree to our{" "}
-        <a href="#" className="underline underline-offset-4 hover:text-primary">
-          Terms of Service
-        </a>{" "}
-        and{" "}
-        <a href="#" className="underline underline-offset-4 hover:text-primary">
-          Privacy Policy
-        </a>
-        .
-      </div>
     </div>
   );
 }
