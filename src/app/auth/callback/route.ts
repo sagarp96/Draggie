cf9f6863d28b2d952ad26432723b306a33a01be3
sagarp96 <sagarcool2345@gmail.com>
2025-09-02 22:00:21 +0530

    Gmail signin issue Test.

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -11,7 +11,7 @@ export async function GET(request: NextRequest) {
     const { error } = await supabase.auth.exchangeCodeForSession(code);
 
     if (!error) {
-      return NextResponse.redirect(`${origin}${next}`);
+      return NextResponse.redirect(`${origin}/Redirect`);
     }
   }
 
